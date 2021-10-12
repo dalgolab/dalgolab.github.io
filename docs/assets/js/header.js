07a63a2497f3ca4c449678e18984a244f5421a2f
Yihan Kim <yihan@postech.ac.kr>
2021-10-12 15:44:08 +0900

    apply docs build

--- docs/assets/js/header.js
+++ docs/assets/js/header.js
@@ -4,9 +4,7 @@ window.onload = () => {
   header.setAttribute("id", "particles-js")
   const headerStyle = header.getAttribute("style")
   header.setAttribute("style", headerStyle  + " overflow-y:hidden;")
-  // console.log(header)
   particlesJS.load('particles-js', 'https://dalgolab.github.io/assets/js/particles.json', function() {
-      // console.log('callback - particles.js config loaded');
       const cvs = document.getElementsByClassName("particles-js-canvas-el")[0]
       const css = cvs.getAttribute("style")
       cvs.setAttribute("style", "width: 100%; position: absolute; top: -100px;")
